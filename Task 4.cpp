fcf4809db7bc22acf977bc23cb7500afa042446d
nkemgimb-lab <nkemgimb@gmail.com>
2026-04-06 22:39:33 +0100

    Assignment submission
    Nkemgim Belinda
    NETWORKING AND SECURITY

---
+++ Task 4.cpp
@@ -0,0 +1,13 @@
+#include<iostream >
+using namespace std ;
+int main () {
+int x=10;
+int *p;
+Long addr;
+cout <<"enter address:" ;
+cin >>addr;
+P=(int*) addr;
+cout <<"enter value:" ;
+cin >>*p;
+cout <<"x = <<x;
+} 
